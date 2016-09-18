d95e6c492eee7f51409bb41bcbd1204315a97fe3
Gabriel Estes <gabriel.estes@gmail.com>
2016-09-18 15:27:46 -0400

    Second attempt at blackjack assignment. Create arrays for cards, create prompts, and calculate card sum.

--- blackjack.rb
+++ blackjack.rb
@@ -1,103 +1,107 @@
+#Normal requirements
 
-game_hash = {}
-game_hash['player'] = []
-game_hash['dealer'] = []
+#the output of the program:
+#offer the next optimal move for the user based on:
+#user's two cards
+#dealer's one card
 
-hit_hash = {
-  hard_hit_hash = {
-    [*2..11] => [*5..11],
-    [2, 3] => [12],
-    [*7..11] => [12..16]}
-    => "Hard Hit"
+#Reference the online data
+#use default 1 deck
 
-  soft_hit_hash = {
-    [*2..11] => [*13..17],
-    [*3..6] => [18],
-    [6] => [19],
-    [9, 10] => [18]}
-    => "Soft Hit"
-}
-pair_hash = {
-  pair_hit_hash = {
-    [*8..11] => "Twos"
-    [*9..11] => "Threes"
-    [2, 3, 7, 8, 9, 10, 11] => "Fours"
-    [*2..11] => "Fives"
-    [*8..11] => "Sixes"
-    [9, 11] => "Sevens"}
-    => "Pair Hit"
+#Must use hashes or arrays within a hash
 
-  pair_split_hash = {
-    [*2..7] => "Twos"
-    [*2..8] => "Threes"
-    [4, 5, 6] => "Fours"
-    [*2..7] => "Sixes"
-    [*2..8] => "Sevens"
-    [*2..11] => "Eights"
-    [2, 3, 4, 5, 6, 8, 9] => "Nines"
-    [*2..11] => "Aces"}
-    => ""
-}
+#user must be able to enter J, Q, K, or A as valid values.
 
-def player_card(card, game_hash)
-  if card == "J" || card == "Q" || card == "K"
-    game_hash['player'] << 10
-  elsif card.to_i >= 2 && card.to_i <= 10
-    game_hash['player'] << card.to_i
-  elsif card == "A"
-    game_hash['player'] << 11
+#Step One:
+
+##Prompt user for first input
+  #Run a method to check validity of answer, ask again if not
+  #If valid answer, store in array (translate into 10's or 11's)
+##Prompt user for second input
+  #Run a method to check validity of answer, ask again if not
+  #If valid answer, store in array
+
+#Step two
+##Prompt user for dealer's card
+  #Run a method to check validity of answer, ask again if not
+  #If valid answer, store as variable
+
+#Step three
+##Run method to access the data based on if hand is hard, soft, or a pair
+
+#Step four
+##Return the user's optimal move
+
+#first number in hash is player total
+
+def player_valid?(card, player_cards)
+  card = card.downcase
+  pushed = false
+  until pushed == true
+    if card == "j" || card == "q" || card == "k"
+      player_cards << 10
+      pushed = true
+    elsif card.to_i >= 2 && card.to_i <= 10
+      player_cards << card.to_i
+      pushed = true
+    elsif card == "a" || card == "ace"
+      player_cards << 11
+      pushed = true
+    else
+      puts "Invalid card entry. Please enter a valid card: "
+      card = gets.chomp.downcase
+      pushed = false
+    end
   end
+  puts "The card you entered was #{card.upcase}."
 end
 
-def dealer_card(card, game_hash)
-  if card == "J" || card == "Q" || card == "K"
-    game_hash['dealer'] << 10
-  elsif card.to_i >= 2 && card.to_i <= 10
-    game_hash['dealer'] << card.to_i
-  elsif card == "A"
-    game_hash['dealer'] << 11
-    hard = true
+def dealer_valid?(card, dealer_card_arr)
+  card = card.downcase
+  pushed = false
+  until pushed == true
+    if card == "j" || card == "q" || card == "k"
+      dealer_card_arr << 10
+      pushed = true
+    elsif card.to_i >= 2 && card.to_i <= 10
+      dealer_card_arr << card.to_i
+      pushed = true
+    elsif card == "a" || card == "ace"
+      dealer_card_arr << 11
+      pushed = true
+    else
+      puts "Invalid card entry. Please enter a valid card: "
+      card = gets.chomp.downcase
+      pushed = false
+    end
   end
+  puts "The dealer's card you entered was #{card.upcase}."
 end
 
-def player_sum(game_hash)
-  total = 0
-  print game_hash['player'].each { |x| x += total}
+def player_sum(player_cards, sum)
 end
 
-def hard_hit(dealer_card, hard_hit_hash, total)
-  hard_hit_hash.each { |key| if hard_hit_hash.key.include?(dealer_card)
-    puts "Hit."
-    end }
+def main()
+
+  player_cards = []
+  dealer_card_arr = []
+
+  puts "Enter your first card: (\"K\", \"Q\", and \"J\" for royals, \"A\" for ace.)"
+  player_valid?(gets.chomp, player_cards)
+  first_card = player_cards[0]
+
+  puts "Enter your second card: (\"K\", \"Q\", and \"J\" for royals, \"A\" for ace.)"
+  player_valid?(gets.chomp, player_cards)
+  second_card = player_cards[1]
+  puts "Player array is #{player_cards}. First card is #{first_card}. Second card is #{second_card}."
+
+  puts "Enter the dealer's card: (\"K\", \"Q\", and \"J\" for royals, \"A\" for ace.)"
+  dealer_valid?(gets.chomp, dealer_card_arr)
+  dealer_card = dealer_card_arr[0]
+  puts "Dealer card is #{dealer_card}."
+  sum = 0
+  player_cards.each { |card| sum += card }
+  puts "Sum of player cards is #{sum}."
 end
 
-def soft_hit(dealer_card, soft_hit_hash, total)
-  soft_hit_hash.each { |key, value| if soft_hit_hash.key.include?(dealer_card) && soft_hit_hash.value.include?(total)
-    puts "Hit."
-    end }
-end
-
-# def pairs(pair_hash)
-# end
-
-puts "What card is in your hand? (\"K\" = King, \"Q\" = Queen, \"J\" = Jack, \"A\" = Ace)"
-  card = gets.chomp
-  first_card = card
-  player_card(card, game_hash)
-
-puts "What is your second card?"
-  card = gets.chomp
-  second_card = card
-  player_card(card, game_hash)
-
-puts "What is the dealer's card?"
-  card = gets.chomp
-  dealer_card(card, game_hash)
-  total = player_sum(game_hash)
-  puts "Player's cards: #{game_hash}"
-
-if hard == true
-  hard_hit(dealer_card(card, game_hash), hit_hash, total)
-else
-  soft_hit(dealer_card(car, game_hash), hit_hash, total)
-end
+main
