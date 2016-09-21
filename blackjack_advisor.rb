39811fb71bc676af3571b6599cd91bba7a4f03e6
Gabriel Estes <gabriel.estes@gmail.com>
2016-09-21 17:23:25 -0400

    Add methods to BlackJack, call in main. Works through asking for cards.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,88 +1,95 @@
-hard_array = [
-  { 'hit.' => {
-    5 => [*2..11],
-    6 => [*2..11],
-    7 => [*2..11],
-    8 => [*2..4, *7..11],
-    9 => [*7..11],
-    10 => [10, 11],
-    12 => [2, 3, *7..11],
-    13 => [*7..11],
-    14 => [*7..11],
-    15 => [*7..11],
-    16 => [*7..11]
-  } },
-  { 'double down.' => {
-    8 => [5, 6],
-    9 => [*2..6],
-    10 => [*2..9],
-    11 => [*2..11]
-  } },
-  { 'stand.' => {
-    12 => [4, 5, 6],
-    13 => [*2..6],
-    14 => [*2..6],
-    15 => [*2..6],
-    16 => [*2..6],
-    17 => [*2..11],
-    18 => [*2..11],
-    19 => [*2..11],
-    20 => [*2..11],
-    21 => [*2..11]
-  } }
-]
-soft_array = [
-  'hit.' => {
-    13 => [2, 3, *7..11],
-    14 => [2, 3, *7..11],
-    15 => [2, 3, *7..11],
-    16 => [2, 3, *7..11],
-    17 => [*7..11],
-    18 => [9, 10]
-  },
-  "double down." => {
-    13 => [4, 5, 6],
-    14 => [4, 5, 6],
-    15 => [4, 5, 6],
-    16 => [4, 5, 6],
-    17 => [*2..6]
-  },
-  'stand unless you can double down.' => {
-    18 => [*3..6],
-    19 => [6]
-  },
-  'stand.' => {
-    18 => [2, 7, 8, 11],
-    19 => [*2..5, *7..11],
-    20 => [*2..11],
-    21 => [*2..11]
-  }
-]
-pair_array = [
-  { 'hit.' => {
-    2 => [*8..11],
-    3 => [9, 10, 11],
-    4 => [2, 3, *7..11],
-    5 => [10, 11],
-    6 => [*8..11],
-    7 => [9, 11]
-  }},
-  {'double down.' => {
-    5 => [*2..9]
-  }},
-  {'stand'. => {
-    7 => [10],
-    9 => [7, 10, 11],
-    10 => [*2..11]
-  } },
-  {'split.' => {
-    2 => [*2..7],
-    3 => [*2..8],
-    4 => [4, 5, 6],
-    6 => [*2..7],
-    7 => [*2..8],
-    8 => [*2..11],
-    9 => [*2..6, 8, 9],
-    11 => [*2..11]
-  }}
-]
+require_relative 'deck_one'
+require_relative 'deck_two'
+require_relative 'deck_three'
+
+class BlackJack
+  def initialize
+    @deck = nil
+
+  end
+
+  def start
+    @deck = get_deck_number
+  end
+
+  def get_deck_number
+    puts "How many decks would you like to play with? (1, 2, or 4)"
+    @deck = gets.chomp.to_i
+    if @deck < 1 || @deck == 3
+      puts 'Please enter a valid deck number. '
+      get_deck_number
+    end
+    @deck
+  end
+
+  def open_deck
+    if @deck == 1
+      @deck = DeckOne.new
+    elsif @deck == 2
+      @deck = DeckTwo.new
+    elsif @deck >= 4
+      @deck = DeckThree.new
+    end
+      @deck.hard_hand
+  end
+
+  def get_cards
+    ask_first_card
+    verify_card
+    @first_card = @card
+    ask_second_card
+    verify_card
+    @second_card = @card
+    ask_dealer_card
+    verify_card
+    @dealer_card = @card
+  end
+
+  def verify_card
+    @card = gets.chomp.downcase
+    if @card == 'j' || @card == 'q' || @card == 'k'
+      @card = 10
+    elsif @card == 'a'
+    elsif @card.to_i >= 2 || @card.to_i <= 10
+      @card = @card.to_i
+    else
+      "Not a valid card."
+      get_cards
+    end
+  end
+
+  def ask_first_card
+    puts "What is the first card in your hand?"
+  end
+
+  def ask_second_card
+    puts "What is the second card in your hand?"
+  end
+
+  def ask_dealer_card
+    puts "What is the dealer's card?"
+  end
+end
+
+def main
+
+  game = BlackJack.new #creates new instance of this class.
+
+  game.start #execute start method. Sets @deck the return of get_deck_number
+
+  game.open_deck #sets deck to new instance of DeckNumber
+
+  game.ask_first_card
+  game.verify_card
+
+  game.ask_second_card
+  game.verify_card
+
+  game.ask_dealer_card
+  game.verify_card
+
+  game.advice
+
+end
+
+main if __FILE__ == $PROGRAM_NAME
